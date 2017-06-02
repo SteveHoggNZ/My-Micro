81d8be93e3e4bdef45ac765b9ca5dbb5971fbaf0
Stephen Hogg <steve@h4.co.nz>
2017-06-03 00:11:20 +1200

    fix (srv-hello): test SAM deploy with CloudFormation Change Set

--- my-micro/services/srv-hello/index.js
+++ my-micro/services/srv-hello/index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 exports.hello = (event, context, callback) => {
-  const msg = 'Hello World!'
+  const msg = 'Hello World!!!!!!!!!!!!!!'
 
   console.log(msg)
   callback(null, msg)
