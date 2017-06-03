e38ba29d8d48133fdb4c0fb365f77a5edd70cbb7
Stephen Hogg <steve@h4.co.nz>
2017-06-03 21:58:09 +1200

    fix (svc-lambda-release): testing for blue green release

--- my-cd/services/svc-lambda-release/src/index.js
+++ my-cd/services/svc-lambda-release/src/index.js
@@ -6,6 +6,8 @@ const log = require('./logging.js')
 exports.pre_release = (event, context, callback) => {
   const info = JSON.parse(event.body)
 
+  console.log('got info', info)
+
   const response = {
     statusCode: 200,
     body: JSON.stringify({
