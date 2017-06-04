431cc01d1af5cccb196990e4d94f8d32b43f8918
Stephen Hogg <steve@h4.co.nz>
2017-06-04 20:14:17 +1200

    debug (svc-lambda-release): test build step failure

--- my-cd/services/svc-lambda-release/src/logging.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  info: function() {
-    console.log.apply(console, Array.prototype.slice.call(arguments))
-  },
-  error: function() {
-    console.error.apply(console, Array.prototype.slice.call(arguments))
-  }
-}
