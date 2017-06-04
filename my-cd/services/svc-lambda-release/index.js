32f76519adc48e591ef615be88507feda104c20c
Stephen Hogg <steve@h4.co.nz>
2017-06-04 20:56:13 +1200

    debug (svc-lambda-release): test build step failure

--- my-cd/services/svc-lambda-release/index.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-exports.hello = (event, context, callback) => {
-  const msg = 'Hi There World!'
-
-  console.log(msg)
-  callback(null, msg)
-}
---
+++ my-cd/services/svc-lambda-release/src/release/index.js
@@ -0,0 +1,77 @@
+'use strict'
+
+var aws = require( '../vendor/aws.js' )
+const log = require('../logging')
+
+
+const codepipeline = aws.CodePipeline()
+const cloudformation = aws.CloudFormation()
+
+
+module.exports.putJobSuccess = ({ jobId }) => {
+  const params = {
+    jobId: jobId
+  }
+
+  return new Promise(function(resolve, reject) {
+    codepipeline.putJobSuccessResult(params, function(err, data) {
+      if (err) {
+        reject(err)
+      } else {
+        resolve(data)
+      }
+    })
+  })
+}
+
+module.exports.putJobFailure = ({ jobId, invokeid }) => {
+  const params = {
+    failureDetails: {
+      message: 'Release failure',
+      type: 'ConfigurationError',
+      externalExecutionId: invokeid
+    },
+    jobId: jobId
+  }
+
+  return new Promise(function(resolve, reject) {
+    codepipeline.putJobFailureResult(params, function(err, data) {
+      if (err) {
+        reject(err)
+      } else {
+        resolve(data)
+      }
+    })
+  })
+}
+
+module.exports.listStackResources = () => {
+  // TO DO: get the correct stack
+  const params = {
+    StackName: 'my-cd-svc-lambda-release-dev'
+  }
+
+  return new Promise(function(resolve, reject) {
+    cloudformation.listStackResources(params, function(err, data) {
+      if (err) reject(err)
+      else resolve(data)
+    })
+  })
+}
+
+module.exports.makePrerelease = ({
+  putJobSuccess = module.exports.putJobSuccess,
+  putJobFailure = module.exports.putJobFailure,
+  listStackResources = module.exports.listStackResources
+} = {}) => ({ jobId, invokeid }) => {
+    return listStackResources()
+      .then((data) => {
+        log.info('listStackResources', data)
+        // return putJobSuccess({ jobId })
+        return putJobFailure({ jobId, invokeid })
+      })
+      .catch((error) => {
+        log.error('listStackResources error', error)
+        return putJobFailure({ jobId, invokeid })
+      })
+  }
