ecf8f25073b3c7c09513b9a93f2fb4825b89b9c3
Stephen Hogg <steve@h4.co.nz>
2017-06-06 21:27:37 +1200

    feat (svc-lambda-release): attempt to publish a version

--- my-cd/services/svc-lambda-release/src/api/handler.js
+++ my-cd/services/svc-lambda-release/src/api/handler.js
@@ -9,14 +9,13 @@ module.exports.prerelease = (event, context, callback) => {
   log.info('prerelease event', JSON.stringify(event))
 
   const jobId = event["CodePipeline.job"].id
+  const UserParameters = JSON.parse(event["CodePipeline.job"].data.actionConfiguration.configuration.UserParameters)
+
   const invokeid = context.invokeid
 
   const prerelease = release.makePrerelease()
 
-  prerelease({ jobId, invokeid })
-    .catch((error) => {
-      callback(error)
-    })
+  prerelease({ jobId, invokeid, versionId: UserParameters['Version'] })
     .then((data) => {
       const response = {
         statusCode: 200,
@@ -28,16 +27,34 @@ module.exports.prerelease = (event, context, callback) => {
       }
       callback(null, response)
     })
+    .catch((error) => {
+      callback(error)
+    })
 }
 
 module.exports.release = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: `Successfully released version ${process.env.SVC_VERSION}`,
-      input: event
-    })
-  }
+  log.info('release event', JSON.stringify(event))
 
-  callback(null, response)
+  const jobId = event["CodePipeline.job"].id
+  const UserParameters = JSON.parse(event["CodePipeline.job"].data.actionConfiguration.configuration.UserParameters)
+
+  const invokeid = context.invokeid
+
+  const release = release.makeRelease()
+
+  release({ jobId, invokeid, versionId: UserParameters['Version'] })
+    .then((data) => {
+      const response = {
+        statusCode: 200,
+        body: JSON.stringify({
+          message: `Successfully released version ${process.env.SVC_VERSION}`,
+          input: event,
+          data: data
+        })
+      }
+      callback(null, response)
+    })
+    .catch((error) => {
+      callback(error)
+    })
 }
--- my-cd/services/svc-lambda-release/src/release/index.js
+++ my-cd/services/svc-lambda-release/src/release/index.js
@@ -73,31 +73,143 @@ module.exports.publishVersion = ({ FunctionName }) => {
   })
 }
 
+exports.listAliases = ({ FunctionName, FunctionVersion = null }) => {
+  const params = {
+    FunctionName,
+    FunctionVersion
+  }
+
+  return new Promise(function(resolve, reject) {
+    lambda.listAliases(params, function(err, data) {
+      if (err) reject(err)
+      else resolve(data)
+    })
+  })
+}
+
+exports.createAlias = ({ FunctionName, FunctionVersion, Name }) => {
+  const params = {
+    FunctionName,
+    FunctionVersion,
+    Name
+  }
+
+  return new Promise(function(resolve, reject) {
+    lambda.createAlias(params, function(err, data) {
+      if (err) reject(err)
+      else resolve(data)
+    })
+  })
+}
+
+exports.createAlias = ({ FunctionName, FunctionVersion, Name }) => {
+  const params = {
+    FunctionName,
+    FunctionVersion,
+    Name
+  }
+
+  return new Promise(function(resolve, reject) {
+    lambda.createAlias(params, function(err, data) {
+      if (err) reject(err)
+      else resolve(data)
+    })
+  })
+}
+
+exports.updateAlias = ({ FunctionName, FunctionVersion, Name }) => {
+  const params = {
+    FunctionName,
+    FunctionVersion,
+    Name
+  }
+
+  return new Promise(function(resolve, reject) {
+    lambda.updateAlias(params, function(err, data) {
+      if (err) reject(err)
+      else resolve(data)
+    })
+  })
+}
+
 module.exports.makePrerelease = ({
   putJobSuccess = module.exports.putJobSuccess,
   putJobFailure = module.exports.putJobFailure,
   listStackResources = module.exports.listStackResources,
-  publishVersion = module.exports.publishVersion
-} = {}) => ({ jobId, invokeid }) => {
+  publishVersion = module.exports.publishVersion,
+  createAlias = exports.createAlias,
+  updateAlias = exports.updateAlias
+} = {}) => ({ jobId, invokeid, versionId }) => {
+    // To Do: set functionName below instead of hard-coding it
+    let result
+    let functionName = 'my-cd-svc-lambda-release-dev-PreRelease'
+
     return listStackResources()
       .then((data) => {
-        // data.StackResourceSummaries.filter()
-
         log.info('listStackResources', data)
 
-        return publishVersion({ FunctionName: 'my-cd-svc-lambda-release-dev-PreRelease' })
-          .then((publishData) => {
-            log.info('publishVersion', publishData)
+        // To Do: set functionName here
+        // data.StackResourceSummaries.filter()
 
-            return putJobFailure({ jobId })
+        return publishVersion({ FunctionName: functionName })
+      })
+      .then((publishData) => {
+        const version = publishData.Version
+
+        result = {
+          ...result,
+          version,
+          versionId
+        }
+
+        log.info(`Published ${versionId} as version ${version}`)
+
+        return updateAlias({ FunctionName: functionName, FunctionVersion: version, Name: 'green' })
+          .catch((updateError) => {
+            log.info('updateAlias error caught', updateError)
+            return createAlias({ FunctionName: functionName, FunctionVersion: version, Name: 'green' })
           })
-          .catch((publishError) => {
-            log.error('publishVersion error', publishError)
-            return putJobFailure({ jobId, invokeid })
-          })
+      })
+      .then((aliasData) => {
+        log.info('createAlias', aliasData)
+
+        return putJobFailure({ jobId, invokeid })
+          .then(() => result)
       })
       .catch((error) => {
-        log.error('listStackResources error', error)
+        log.error('Prerelease error', error)
         return putJobFailure({ jobId, invokeid })
+          .then(() => result)
       })
   }
+
+  exports.makeRelease = ({
+    putJobSuccess = module.exports.putJobSuccess,
+    putJobFailure = module.exports.putJobFailure,
+    listStackResources = module.exports.listStackResources,
+    listAliases = exports.listAliases,
+    createAlias = exports.createAlias,
+    updateAlias = exports.updateAlias
+  } = {}) => ({ jobId, invokeid, versionId }) => {
+      // To Do: set functionName below instead of hard-coding it
+      let functionName = 'my-cd-svc-lambda-release-dev-PreRelease'
+
+      return listStackResources()
+        .then((data) => {
+          log.info('listStackResources', data)
+
+          // To Do: set functionName here
+          // data.StackResourceSummaries.filter()
+
+          return listAliases({ FunctionName: functionName })
+        })
+        .then((aliasData) => {
+          log.info('Release aliasData', aliasData)
+
+          return putJobFailure({ jobId, invokeid })
+        })
+        .catch((error) => {
+          log.error('Release error', error)
+          return putJobFailure({ jobId, invokeid })
+        })
+    }
