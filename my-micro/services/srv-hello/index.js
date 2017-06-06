c7e6ffec9c78b7959b6cbe151b44fe8d6094f134
Stephen Hogg <steve@h4.co.nz>
2017-06-06 22:08:55 +1200

    feat (srv-hello): add manual approval step to SAM-basic

--- my-micro/services/srv-hello/index.js
+++ my-micro/services/srv-hello/index.js
@@ -1,7 +1,21 @@
-'use strict';
+'use strict'
 
-exports.hello = (event, context, callback) => {
-  const msg = 'Hi There World!'
+exports.hello_nz = (event, context, callback) => {
+  const msg = 'HelloWorld NZ! 1'
+
+  console.log(msg)
+  callback(null, msg)
+}
+
+exports.hello_world = (event, context, callback) => {
+  const msg = 'HelloWorld World! 1'
+
+  console.log(msg)
+  callback(null, msg)
+}
+
+exports.hello_universe = (event, context, callback) => {
+  const msg = 'HelloWorld Universe! 1'
 
   console.log(msg)
   callback(null, msg)
