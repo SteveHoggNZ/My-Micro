b7e8f75f78ced223de26ba8a58d293b44cfa7196
Stephen Hogg <steve@h4.co.nz>
2017-06-06 23:33:40 +1200

    feat (svc-hello): update NZ function

--- my-micro/services/svc-hello/index.js
+++ my-micro/services/svc-hello/index.js
@@ -1,7 +1,7 @@
 'use strict'
 
 exports.hello_nz = (event, context, callback) => {
-  const msg = 'HelloWorld NZ! 1'
+  const msg = 'HelloWorld NZ! 3'
 
   console.log(msg)
   callback(null, msg)
@@ -15,7 +15,7 @@ exports.hello_world = (event, context, callback) => {
 }
 
 exports.hello_universe = (event, context, callback) => {
-  const msg = 'HelloWorld Universe! 1'
+  const msg = 'HelloWorld Universe! 2'
 
   console.log(msg)
   callback(null, msg)
