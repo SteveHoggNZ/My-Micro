fa6dc4f5daf975564796e47c27aa7640ac52a77d
Stephen Hogg <steve@h4.co.nz>
2017-06-07 12:10:42 +1200

    fix (cd-infrastructure): add environment setting

--- my-cd/services/cd-infrastructure/deploy.sh
+++ my-cd/services/cd-infrastructure/deploy.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+SVC_ENVIRONMENT=dev
+
 set -x
 
-aws cloudformation update-stack --stack-name my-cd-dev --template-body file://cf.yml --parameters ParameterKey=SvcUrlParameter,ParameterValue=https://github.com/SteveHoggNZ/my-repo.git ParameterKey=ProjectNameParameter,ParameterValue=my-cd --capabilities CAPABILITY_NAMED_IAM
+aws cloudformation update-stack --stack-name my-cd-${SVC_ENVIRONMENT} \
+  --template-body file://cf.yml \
+  --parameters ParameterKey=SvcUrlParameter,ParameterValue=https://github.com/SteveHoggNZ/my-repo.git \
+              ParameterKey=ProjectNameParameter,ParameterValue=my-cd \
+              ParameterKey=EnvironmentParameter,ParameterValue=${SVC_ENVIRONMENT} \
+  --capabilities CAPABILITY_NAMED_IAM
